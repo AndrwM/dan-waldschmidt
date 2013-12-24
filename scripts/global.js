4acc1f380010a14e2fe32f5cad4f82952e91f6e4
Andrew McIntee <me@andrewmcintee.com>
2013-12-24 01:30:05 -0500

    Blog listing page done

--- scripts/global.js
+++ scripts/global.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
 
   if ($('body.blog-single')[0]){
 
-    $('.post-details').waypoint('sticky');
+    $('.post-details').waypoint('sticky', {
+      wrapper: '<div class="post-details-placeholder" />',
+      stuckClass: 'fixed'
+    });
     
   };
 
